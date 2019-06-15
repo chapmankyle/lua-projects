f0804377822d309cdc4a0465dbaac3a2b3123c9f
Kyle Chapman <20703236@sun.ac.za>
2019-06-15 17:37:18 +0200

    Optimized pause screen

--- basics/main.lua
+++ basics/main.lua
@@ -39,46 +39,21 @@ function love.load()
 
 	-- options when paused
 	pauseOpts = {
-		continue = {
-			text = "Continue",
-			x = 30,
-			y = 200,
-			show = function()
-				return "Continue", 30, 200
-			end
-		},
-		newGame = {
-			text = "New Game",
-			x = 30,
-			y = 240,
-			show = function()
-				return "New Game", 30, 240
-			end
-		},
-		options = {
-			text = "Options",
-			x = 30,
-			y = 280,
-			show = function()
-				return "Options", 30, 280
-			end
-		},
-		quitMain = {
-			text = "Quit to Main Menu",
-			x = 30,
-			y = 320,
-			show = function()
-				return "Quit to Main Menu", 30, 320
-			end,
-		},
-		quitDesktop = {
-			text = "Quit to Desktop",
-			x = 30,
-			y = 360,
-			show = function()
-				return "Quit to Desktop", 30, 360
-			end
-		}
+		continue = function()
+			return "Continue", 30, 200
+		end,
+		newGame = function()
+			return "New Game", 30, 240
+		end,
+		options = function()
+			return "Options", 30, 280
+		end,
+		quitMain = function()
+			return "Quit to Main Menu", 30, 320
+		end,
+		quitDesktop = function()
+			return "Quit to Desktop", 30, 360
+		end
 	}
 
 	-- entities
@@ -173,43 +148,41 @@ end
 
 -- checks input on hover
 function checkOptionHover()
-	love.mouse.setCursor(defCursor)
+	local ctText, ctX, ctY = pauseOpts.continue()
+	local ngText, ngX, ngY = pauseOpts.newGame()
+	local optText, optX, optY = pauseOpts.options()
+	local qmText, qmX, qmY = pauseOpts.quitMain()
+	local qdText, qdX, qdY = pauseOpts.quitDesktop()
 
-	local ctText, ctX, ctY = pauseOpts.continue.show()
 	if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, ctX, ctY,
 			defaultFont:getWidth(ctText), defaultFont:getHeight(ctText)) then
 		love.mouse.setCursor(handCursor)
-	end
-
-	local ngText, ngX, ngY = pauseOpts.newGame.show()
-	if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, ngX, ngY,
+	elseif checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, ngX, ngY,
 			defaultFont:getWidth(ngText), defaultFont:getHeight(ngText)) then
 		love.mouse.setCursor(handCursor)
-	end
-
-	local optText, optX, optY = pauseOpts.options.show()
-	if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, optX, optY,
+	elseif checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, optX, optY,
 			defaultFont:getWidth(optText), defaultFont:getHeight(optText)) then
 		love.mouse.setCursor(handCursor)
-	end
-
-	local qmText, qmX, qmY = pauseOpts.quitMain.show()
-	if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, qmX, qmY,
+	elseif checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, qmX, qmY,
 			defaultFont:getWidth(qmText), defaultFont:getHeight(qmText)) then
 		love.mouse.setCursor(handCursor)
-	end
-
-	local qdText, qdX, qdY = pauseOpts.quitDesktop.show()
-	if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, qdX, qdY,
+	elseif checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, qdX, qdY,
 			defaultFont:getWidth(qdText), defaultFont:getHeight(qdText)) then
 		love.mouse.setCursor(handCursor)
+	else
+		love.mouse.setCursor(defCursor)
 	end
 end
 
 -- check for mouse click on one of the options buttons
 function checkOptionClick()
 	if love.mouse.isDown('1') then
-		local ctText, ctX, ctY = pauseOpts.continue.show()
+		local ctText, ctX, ctY = pauseOpts.continue()
+		local ngText, ngX, ngY = pauseOpts.newGame()
+		local optText, optX, optY = pauseOpts.options()
+		local qmText, qmX, qmY = pauseOpts.quitMain()
+		local qdText, qdX, qdY = pauseOpts.quitDesktop()
+
 		if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, ctX, ctY,
 				defaultFont:getWidth(ctText), defaultFont:getHeight(ctText)) then
 			doPause()
@@ -217,28 +190,24 @@ function checkOptionClick()
 			return
 		end
 
-		local ngText, ngX, ngY = pauseOpts.newGame.show()
 		if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, ngX, ngY,
 				defaultFont:getWidth(ngText), defaultFont:getHeight(ngText)) then
 			print("Pressed New Game")
 			return
 		end
 
-		local optText, optX, optY = pauseOpts.options.show()
 		if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, optX, optY,
 				defaultFont:getWidth(optText), defaultFont:getHeight(optText)) then
 			print("Pressed Options")
 			return
 		end
 
-		local qmText, qmX, qmY = pauseOpts.quitMain.show()
 		if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, qmX, qmY,
 				defaultFont:getWidth(qmText), defaultFont:getHeight(qmText)) then
 			print("Pressed Quit to Main Menu")
 			return
 		end
 
-		local qdText, qdX, qdY = pauseOpts.quitDesktop.show()
 		if checkCollision(love.mouse.getX(), love.mouse.getY(), 10, 10, qdX, qdY,
 				defaultFont:getWidth(qdText), defaultFont:getHeight(qdText)) then
 			love.event.push('quit')
@@ -321,11 +290,11 @@ function drawPause()
 
 	-- draw options
 	love.graphics.setFont(defaultFont)
-	love.graphics.print(pauseOpts.continue.show())
-	love.graphics.print(pauseOpts.newGame.show())
-	love.graphics.print(pauseOpts.options.show())
-	love.graphics.print(pauseOpts.quitMain.show())
-	love.graphics.print(pauseOpts.quitDesktop.show())
+	love.graphics.print(pauseOpts.continue())
+	love.graphics.print(pauseOpts.newGame())
+	love.graphics.print(pauseOpts.options())
+	love.graphics.print(pauseOpts.quitMain())
+	love.graphics.print(pauseOpts.quitDesktop())
 
 	love.graphics.pop()
 end
